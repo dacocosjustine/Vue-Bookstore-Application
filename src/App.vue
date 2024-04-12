407f4882c46bb9ecbe23aa19a590adec3679e33f
Sam Reginald Besa <besasamreginald36@gmail.com>
2024-04-12 20:28:07 +0800

    test commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <div class="logo">
-      <a>Online Botore</a>
+      <a>Online Bookstore</a>
     </div>
     <router-link v-if="isAuthenticated === true" to="/">Book List</router-link>
     <router-link v-if="cartLength > 0" to="/cart">In Cart ({{ cartLength }})</router-link>
